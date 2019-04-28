68986b170908d75d1178d90d4a7baab3dee32ffb
Maxime Salomon <maxime@croissant.io>
2019-04-28 21:01:21 +0200

    Create lambda questions 🧠

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const questions = [
+  {
+    question: "What's the latest version of HTML?",
+    options: ["HTML3", "HTML4", "HTML5", "HTML6"],
+    answer: "HTML5"
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1 id="question">{questions[0].question}</h1>
+      <li>{
+        questions[0].options.forEach(option => {
+          return <ul>{option}</ul>;
+        })
+      }
+      </li>
+    </main>
   );
 }
 
